6c3ca5abe6cda4cb1beeaab0f56369eb30fa1340
Szymon Nieradka <szymon[]nieradka.net>
2017-05-21 15:30:03 +0200

    yyy

---
+++ form/inc/include.php
@@ -0,0 +1,65 @@
+<?PHP
+
+$host = $_SERVER['SERVER_NAME'];
+
+$categories = Array(
+	4  => "Zastawienie chodnika (mniej niż 1.5m)",
+	2  => "Mniej niż 15 m od przystanku",
+	3  => "Mniej niż 10m od skrzyżowania",
+	9  => "Blokowanie ścieżki rowerowej	",
+	5  => "Mniej niż 10m od przejścia dla pieszych",
+	6  => "Parkowanie na trawniku/w parku",
+	//10 => "Parkowanie za barierkami",
+	//8  => "Parkowanie z dala od krawędzi jezdni",
+	//7  => "Niszczenie chodnika",
+	0 => "Inne"
+);
+
+$categoriesMatrix = Array( 'a', 'b', 'c' );
+
+function genHeader($title = "Uprzejmie Donoszę"){
+	echo <<<HTML
+<!DOCTYPE html>
+<html lang="pl">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<title>$title</title>
+		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon"                 href="img/apple-touch-icon.png" />
+		<link rel="apple-touch-icon" sizes="57x57"   href="img/apple-touch-icon-57x57.png" />
+		<link rel="apple-touch-icon" sizes="72x72"   href="img/apple-touch-icon-72x72.png" />
+		<link rel="apple-touch-icon" sizes="76x76"   href="img/apple-touch-icon-76x76.png" />
+		<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png" />
+		<link rel="apple-touch-icon" sizes="120x120" href="img/apple-touch-icon-120x120.png" />
+		<link rel="apple-touch-icon" sizes="144x144" href="img/apple-touch-icon-144x144.png" />
+		<link rel="apple-touch-icon" sizes="152x152" href="img/apple-touch-icon-152x152.png" />
+		<link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon-180x180.png" />
+		
+		<meta property="og:image" content="https://$host/img/uprzejmiedonosze.png"/>
+		<meta property="og:title" content="$title"/>
+		<meta property="og:description" content="Uprzejmie Donoszę pozwala na przekazywanie zgłoszeń o sytuacjach które wpływają na komfort i bezpieczeństwo pieszych. Umożliwia ona w wygodny sposób wykonać zgłoszenie i przekazać jest bezpośrednio Straży Miejskiej."/>
+		<meta property="og:url" content="https://$host.net"/>
+		<meta property="og:locale" content="pl_PL" />
+		<meta property="og:type" content="website" />
+
+		<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+		<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+		<script src="js/script.js"></script>
+		<link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+		<link rel="stylesheet" href="css/style.css">
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+			ga('create', 'UA-99241725-1', 'auto');
+			ga('send', 'pageview');
+		</script>
+	</head>
+HTML;
+}
+
+?>
---
+++ form/js/script.js
@@ -0,0 +1,199 @@
+$(document).on('pageinit pageshow', function() {
+	if($("#address").length){
+		$("a#geo").on('touchstart click', getAddress);
+		getAddress();
+		$('#address').on('change', function(){
+			$('#address').removeClass('error');
+		});
+		$('#plateid').on('change', function(){
+			$('#plateid').removeClass('error');
+		});
+	}
+	if($('#pic1').length){
+		if (window.File && window.FileReader && window.FormData) {
+			$(document).on('change', "#pic1", function (e) {
+				checkFile(e.target.files[0], this.id);
+			});
+			$(document).on('change', "#pic2", function (e) {
+				checkFile(e.target.files[0], this.id);
+			});
+		} else {
+		}
+	}
+	if($('#form-submit').length){
+		$('#form-submit').click(function(){
+			if(validateForm()){
+				$('#form').submit();
+			}
+		});
+	}
+});
+
+function validateForm(){
+	var ret = check($('#plateid'), 6);
+	ret = check($('#address'), 10) && ret;
+	ret = check($('#pic2'), 0, true) && ret;
+	ret = check($('#pic1'), 0, true) && ret;;
+	if(!ret){
+		$(window).scrollTop($('.error').offset().top - 100);
+	}
+	return ret;
+	function check(item, length, grandma = false){
+		if(item.val().trim().length <= length){
+			if(grandma){
+				item.parent().parent().addClass('error');
+			}else{
+				item.addClass('error');
+			}
+			return false;
+		}else{
+			return true;
+		}
+	}
+}
+
+function getAddress(){
+	$('a#geo').buttonMarkup({ icon: "clock" });
+	$('#address').val("Pobieram adres...");
+	if ("geolocation" in navigator) {
+
+		var geoOptions = {
+			enableHighAccuracy: true, 
+			maximumAge        : 30000, 
+			timeout           : 27000
+		};
+
+		navigator.geolocation.getCurrentPosition(function(position) {
+			var latitude = position.coords.latitude;
+			var longitude = position.coords.longitude;
+			var latlng = latitude + ',' + longitude;
+
+			$.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" 
+				+ latlng + "&key=AIzaSyAsVCGVrc7Zph5Ka3Gh2SGUqDrwCd8C3DU&language=pl&result_type=street_address", function(data){
+					$('#address').val(data.results[0].formatted_address.replace(', Polska', ''));
+					$('a#geo').buttonMarkup({ icon: "refresh" });
+					$('#latlng').val(latlng);
+				});
+
+		}, function(error){
+			$('a#geo').buttonMarkup({ icon: "alert" });
+			$('#address').val("");
+		}, geoOptions);
+	} else {
+		$('a#geo').buttonMarkup({ icon: "alert" });
+	}
+}
+
+function checkFile(file, id){
+	$('#' + id).parent().parent().removeClass('error');
+	$('img#img' + id).attr("src", 'img/loading.gif');
+	if(id == 'pic2'){
+		$('#imgpic2Plate').attr("src", "");
+	}
+	if (file) {
+		if (/^image\//i.test(file.type)) {
+			readFile(file, id);
+		} else {
+			$('img#img' + id).attr("src", 'img/camera.png');
+			$('#' + id).textinput('enable');
+		}
+	}
+}
+
+function readFile(file, id) {
+	var reader = new FileReader();
+
+	reader.onloadend = function () {
+		processFile(reader.result, file.type, id);
+	}
+
+	reader.onerror = function () {
+		$('img#img' + id).attr("src", 'img/camera.png');
+		$('#' + id).textinput('enable');
+	}
+
+	reader.readAsDataURL(file);
+}
+function processFile(dataURL, fileType, id) {
+	var maxWidth = 1200;
+	var maxHeight = 1200;
+
+	var image = new Image();
+	image.src = dataURL;
+
+	image.onload = function () {
+		var width = image.width;
+		var height = image.height;
+		var shouldResize = (width > maxWidth) || (height > maxHeight);
+
+		if (!shouldResize) {
+			sendFile(dataURL, id);
+			return;
+		}
+
+		var newWidth;
+		var newHeight;
+
+		if (width > height) {
+			newHeight = height * (maxWidth / width);
+			newWidth = maxWidth;
+		} else {
+			newWidth = width * (maxHeight / height);
+			newHeight = maxHeight;
+		}
+
+		var canvas = document.createElement('canvas');
+
+		canvas.width = newWidth;
+		canvas.height = newHeight;
+
+		var context = canvas.getContext('2d');
+
+		context.drawImage(this, 0, 0, newWidth, newHeight);
+
+		dataURL = canvas.toDataURL(fileType);
+		sendFile(dataURL, id);
+	};
+
+	image.onerror = function () {
+		$('img#img' + id).attr("src", 'img/camera.png');
+		$('#' + id).textinput('enable');
+	};
+}
+
+function sendFile(fileData, id) {
+	var formData = new FormData();
+
+	formData.append('image_data', fileData);
+	formData.append('id', id);
+
+	$.ajax({
+		type: 'POST',
+		url: '/upload.html',
+		data: formData,
+		contentType: false,
+		processData: false,
+		success: function (data) {
+			json = $.parseJSON(data);
+			if(json.thumbUrl) {
+				$('img#img' + id).attr("src", json.thumbUrl);
+				$('#' + id).textinput('disable');
+				$('#' + id + 'Url').val(json.imageUrl);
+				$('#' + id + 'Thumb').val(json.thumbUrl);
+			} else {
+			}
+			if(id == 'pic2' && json.plate) {
+				$('#plateid').val(json.plate);
+			} else {
+			}
+			if(id == 'pic2' && json.plateImage){
+				$('#imgpic2Plate').attr("src", json.plateImage);
+			}else{
+			}
+		},
+		error: function (data) {
+			$('img#img' + id).attr("src", 'img/camera.png');
+			$('#' + id).textinput('enable');
+		}
+	});
+}
