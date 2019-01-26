f59bef0d2be53c14b0ada11efff375547059003c
Szymon Nieradka <szymon[]nieradka.net>
2019-01-26 13:47:09 +0100

    nowy piękny Application.php

--- form/src/inc/Application.php
+++ form/src/inc/Application.php
@@ -3,27 +3,36 @@
 require_once(__DIR__ . '/Utils.php');
 require_once(__DIR__ . '/JSONObject.php');
 
+/**
+ * Application class.
+ */
 class Application extends JSONObject{
+    /**
+     * Creates new Application of initites it from JSON.
+     */
     public function __construct($json = null) {
-        logger("Application::__construct");
         if($json){
-            logger("Application::__construct with json");
             parent::__construct($json);
-        }else{
-            logger("Application::__construct no json");
-            global $storage;
-
-            $this->id = guidv4();
-            $this->added = date(DT_FORMAT);
-            $this->user = $storage->getCurrentUser()->data;
-            $this->status = 'draft';
+            return;
         }
+        global $storage;
+        $this->id = guidv4();
+        $this->added = date(DT_FORMAT);
+        $this->user = $storage->getCurrentUser()->data;
+        $this->status = 'draft';
+        
     }
 
+    /**
+     * Returns application date in Y-m-d format.
+     */
     public function getDate(){
         return (new DateTime($this->date))->format('Y-m-d');
     }
 
+    /**
+     * Returns application time in H:i format.
+     */
     public function getTime(){
         return (new DateTime($this->date))->format('H:i');
     }
