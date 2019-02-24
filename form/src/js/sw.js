7cb7ca339c020d0b0fedc65a4bb98516fd72aca7
Szymon Nieradka <szymon[]nieradka.net>
2019-02-24 18:27:55 +0100

    nic nie rozumiem z service workerów

--- form/src/js/sw.js
+++ form/src/js/sw.js
@@ -1,68 +1,99 @@
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open('uprzejmiedonosze').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/?homescreen=1',
-                '/index.html',
-                '/index.html?homescreen=1',
-                '/projekt.html',
-                '/robtodobrze.html',
-                '/changelog.html',
-                '/przepisy.html',
+const LATEST_CACHE_ID = 'v1';
 
-                '/img/apple-touch-icon.png',
-                '/img/b36.png',
-                '/img/d18.png',
-                '/img/d40.png',
-                '/img/d46.png',
-                '/img/d52.png',
-                '/img/przyklad-1.jpg',
-                '/img/przyklad-2.jpg',
-                '/img/przyklad-3.jpg',
-                '/img/przyklad-4.jpg',
-                '/img/robtodobrze-radni-s.jpg',
-                '/img/robtodobrze-radni.jpg',
-                '/img/robtodobrze-s.jpg',
-                '/img/robtodobrze.jpg',
-                '/img/rtd-mandat-cmyk-print.pdf',
-                '/img/sm-tvn24-s.jpg',
-                '/img/sm-tvn24.jpg',
-                '/img/splash.png',
-                '/img/t30a.gif',
-                '/img/t30b.gif',
-                '/img/t30c.gif',
-                '/img/t30d.gif',
-                '/img/t30e.gif',
-                '/img/t30f.gif',
-                '/img/t30g.gif',
-                '/img/t30h.gif',
-                '/img/t30i.gif',
-                '/img/uprzejmiedonosze.png',
+self.addEventListener('install', function (evt) {
+    evt.waitUntil(precache());
+});
 
-                '/css/style-%CSS_HASH%.css',
-
-                '/js/script-%JS_HASH%.js',
-                '/js/load-image-%JS_HASH%.js',
-                '/js/load-image-scale-%JS_HASH%.js',
-                '/js/load-image-meta-%JS_HASH%.js',
-                '/js/load-image-orientation-%JS_HASH%.js',
-                '/js/load-image-exif-%JS_HASH%.js',
-                '/js/load-image-exif-map-%JS_HASH%.js'
-
-            ], { mode: 'same-origin', redirect: 'manual' });
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName !== LATEST_CACHE_ID) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(function () {
+            return self.clients.claim();
         })
     );
 });
 
-self.addEventListener('activate',  function(event) {
-    event.waitUntil(self.clients.claim());
+self.addEventListener('fetch', function (evt) {
+    evt.respondWith(fromNetwork(evt.request, 400).catch(function () {
+        return fromCache(evt.request);
+    }));
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
-});
+function precache() {
+    return caches.open(LATEST_CACHE_ID).then(function (cache) {
+        return cache.addAll([
+            '/',
+            '/?homescreen=1',
+            '/index.html',
+            '/index.html?homescreen=1',
+            '/projekt.html',
+            '/robtodobrze.html',
+            '/changelog.html',
+            '/przepisy.html',
+
+            '/img/apple-touch-icon.png',
+            '/img/b36.png',
+            '/img/d18.png',
+            '/img/d40.png',
+            '/img/d46.png',
+            '/img/d52.png',
+            '/img/przyklad-1.jpg',
+            '/img/przyklad-2.jpg',
+            '/img/przyklad-3.jpg',
+            '/img/przyklad-4.jpg',
+            '/img/robtodobrze-radni-s.jpg',
+            '/img/robtodobrze-radni.jpg',
+            '/img/robtodobrze-s.jpg',
+            '/img/robtodobrze.jpg',
+            '/img/rtd-mandat-cmyk-print.pdf',
+            '/img/sm-tvn24-s.jpg',
+            '/img/sm-tvn24.jpg',
+            '/img/splash.png',
+            '/img/t30a.gif',
+            '/img/t30b.gif',
+            '/img/t30c.gif',
+            '/img/t30d.gif',
+            '/img/t30e.gif',
+            '/img/t30f.gif',
+            '/img/t30g.gif',
+            '/img/t30h.gif',
+            '/img/t30i.gif',
+            '/img/uprzejmiedonosze.png',
+
+            '/css/style-%CSS_HASH%.css',
+
+            '/js/script-%JS_HASH%.js',
+            '/js/load-image-%JS_HASH%.js',
+            '/js/load-image-scale-%JS_HASH%.js',
+            '/js/load-image-meta-%JS_HASH%.js',
+            '/js/load-image-orientation-%JS_HASH%.js',
+            '/js/load-image-exif-%JS_HASH%.js',
+            '/js/load-image-exif-map-%JS_HASH%.js'
+        ]);
+    });
+}
+
+function fromNetwork(request, timeout) {
+    return new Promise(function (fulfill, reject) {
+        var timeoutId = setTimeout(reject, timeout);
+        fetch(request).then(function (response) {
+            clearTimeout(timeoutId);
+            fulfill(response);
+        }, reject);
+    });
+}
+
+function fromCache(request) {
+    return caches.open(LATEST_CACHE_ID).then(function (cache) {
+        return cache.match(request).then(function (matching) {
+            return matching || Promise.reject('no-match');
+        });
+    });
+}
