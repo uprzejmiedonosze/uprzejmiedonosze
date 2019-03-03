6a64c3703ec3898d89b896f242f36c5575d8c8b7
Szymon Nieradka <szymon[]nieradka.net>
2019-03-03 12:04:42 +0100

    ustawienia eslint-a

--- form/.eslintrc.js
+++ form/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "extends": "eslint:recommended",
+    "extends": "jquery",
     "parserOptions": {
         "ecmaVersion": 2018
     },
