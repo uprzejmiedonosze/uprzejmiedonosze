c36e648bc0e58ec31402e705b00893c91aa5113c
Szymon Nieradka <szymon[]nieradka.net>
2019-04-15 16:33:15 +0200

    moving files

---
+++ src/inc/include.php
@@ -0,0 +1,56 @@
+<?PHP
+require_once(__DIR__ . '/utils.php');
+session_start();
+
+require(__DIR__ . '/firebase.php');
+
+require_once(__DIR__ . '/DB.php');
+$storage = new DB();
+
+function generate($template, $parameters){
+    global $storage;
+
+    if(!isset($parameters['config'])) $parameters['config'] = [];
+    if(!isset($parameters['config']['menu'])) $parameters['config']['menu'] = '';
+    if(!isset($parameters['head'])) $parameters['head'] = [];
+
+    $isLoggedIn = isLoggedIn();
+    $auth = isset($parameters['config']['auth']);
+    $registration = isset($parameters['config']['register']);
+
+    if($auth){
+        checkIfLogged();
+        if(!$registration){
+            checkIfRegistered();
+        }
+    }    
+    
+    $loader = new Twig_Loader_Filesystem(__DIR__ . '/../templates');
+    $twig = new Twig_Environment($loader,
+    [
+        'debug' => false,
+        'cache' => new Twig_Cache_Filesystem('/tmp/twig%TWIG_HASH%', Twig_Cache_Filesystem::FORCE_BYTECODE_INVALIDATION),
+        'strict_variables' => true,
+        'auto_reload' => true
+    ]);
+
+    $parameters['general'] =
+        [
+            'uri' => $_SERVER['REQUEST_URI'],
+            'isLoggedIn' => $isLoggedIn,
+            'hasApps' => $isLoggedIn && $storage->getCurrentUser()->hasApps(),
+            'isAdmin' => $isLoggedIn && $storage->getCurrentUser()->isAdmin()
+        ];
+    
+    if($isLoggedIn){
+        $parameters['config']['sex'] = guess_sex_current_user();
+        $parameters['general']['userName'] = $storage->getCurrentUser()->data->name;
+        $parameters['general']['stats'] = $storage->getUserStats();
+    }
+    
+    $parameters['statuses'] = STATUSES;
+
+    echo $twig->render($template, $parameters);
+};
+
+?>
---
+++ src/inc/utils.php
@@ -0,0 +1,245 @@
+<?PHP
+require_once(__DIR__ . '/config.php');
+
+function exception_handler($exception) {
+    try{
+        $email = getCurrentUserEmail();
+    }catch(Exception $e){
+        $email = 'niezalogowany';
+    }
+    $msg = $exception->getMessage() . " szkodnik: $email, " . $exception->getFile()
+        . ':' . $exception->getLine() . "\n" . $exception->getTraceAsString();
+    $time = logger($msg, true);
+
+    _sendSlackError($msg);
+    
+    $loader = new Twig_Loader_Filesystem(__DIR__ . '/../templates');
+    $twig = new Twig_Environment($loader,
+    [
+        'debug' => false,
+        'strict_variables' => false
+    ]);
+
+    echo $twig->render('error.html.twig', [
+        'head' =>
+        [
+            'title' => "Wystąpił błąd",
+            'shortTitle' => "Wystąpił błąd"
+        ],
+        'general' =>
+        [
+            'uri' => $_SERVER['REQUEST_URI']
+        ],
+        'msg' => $msg,
+        'exception' => $exception,
+        'email' => $email,
+        'time' => $time
+    ]);
+}
+
+set_exception_handler('exception_handler');
+
+function logger($msg, $force = null){
+    $user = '';
+    if(!empty($_SESSION['user_email'])){
+        $user = " [" . $_SESSION['user_email'] . ']';
+    }
+
+    $time = date(DT_FORMAT);
+    if('%HOST%' != 'uprzejmiedonosze.net' || $force){
+        error_log($time . $user . "\t$msg\n", 3, "/tmp/%HOST%.log");
+    }
+    return $time;
+}
+
+function getCurrentUserEmail(){
+    if(!empty($_SESSION['user_email'])){
+        return $_SESSION['user_email'];
+    }
+    throw new Exception("Próba pobrania danych niezalogowanego użytkownika");
+}
+
+function checkIfLogged(){
+    if(!isLoggedIn()){
+        redirect("login.html?next=" . getRequestUri());
+    }
+}
+
+function getRequestUri(){
+    return preg_replace('/^\/*/', '', $_SERVER['REQUEST_URI']);
+}
+
+function checkIfRegistered(){
+    global $storage;
+    checkIfLogged();
+
+    try {
+        $user = $storage->getCurrentUser();
+    }catch (Exception $e){
+        redirect("register.html?next=" . getRequestUri());
+    }
+    if(!$user){
+        redirect("register.html?next=" . getRequestUri());
+    }
+    if(!$user->isRegistered()) {
+        redirect("register.html?next=" . getRequestUri());
+    }
+}
+
+
+function guidv4(){
+	$data = openssl_random_pseudo_bytes(16);
+    assert(strlen($data) == 16);
+
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+}
+
+/** @SuppressWarnings("exit") */
+function raiseError($msg, $status){
+    logger("raiseError $msg with $status", true);
+    $error = Array(
+        "code" => $status,
+        "message" => $msg
+    );
+    http_response_code($status);
+    echo json_encode($error);
+    die();
+}
+
+function guess_sex_by_name($name){
+    $names = preg_split('/\s+/', mb_strtolower($name, 'UTF-8'));
+    if(count($names) < 1){
+        return '?';
+    }
+    if($names[0] == 'kuba' || substr($names[0], -1) != 'a'){
+        return 'm';
+    }
+    return 'f';
+}
+
+function guess_sex_current_user(){
+    return SEXSTRINGS[guess_sex_by_name($_SESSION['user_name'])];
+}
+
+function capitalizeSentence($input){
+    if(!isset($input) || trim($input) === ''){
+        return '';
+    }
+    $isUpperCase = (mb_strlen($input, 'UTF-8') / 2) < (int)preg_match_all('/[A-Z]/', $input);
+    
+    $out = trim(
+        preg_replace_callback('/([.!?])\s+(\w)/', function ($matches) {
+            return mb_strtoupper($matches[1] . ' ' . $matches[2], 'UTF-8');
+            }, ucfirst( $isUpperCase ? (mb_strtolower($input, 'UTF-8')): $input )
+        )
+    );
+    return (substr($out, -1) == '.')? $out: "{$out}.";
+}
+
+function capitalizeName($input){
+    if(!isset($input) || trim($input) === ''){
+        return '';
+    }
+    return trim(ucwords(mb_strtolower($input)));
+}
+
+function isIOS(){
+    $iPod    = (bool)stripos($_SERVER['HTTP_USER_AGENT'],"iPod");
+    $iPhone  = (bool)stripos($_SERVER['HTTP_USER_AGENT'],"iPhone");
+    $iPad    = (bool)stripos($_SERVER['HTTP_USER_AGENT'],"iPad");
+    return $iPod || $iPhone || $iPad;
+}
+
+/** @SuppressWarnings("exit") */
+function redirect($destPath){
+    $destPath = preg_replace('/\/+/', '/', $destPath);
+
+    logger("redirect to " . $destPath);
+    header("X-Redirect: %HTTPS%://%HOST%/$destPath");
+    header("Location: %HTTPS%://%HOST%/$destPath");
+    die();
+}
+
+/** 
+ * Sends message to #updates slack channel at uprzejmiedonosze.slack.com
+ */
+function _sendSlackOnRegister($user){
+    $title = "Nowa rejestracja {$user->data->name}";
+
+    logger($title, true);
+
+    $url = ('%HOST%' != 'uprzejmiedonosze.net')?
+        'https://hooks.slack.com/services/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': # staging / dev
+        'https://hooks.slack.com/services/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'; # prod
+
+    $msg = [
+        "fallback" => $title,
+        "title" => $title,
+        "color" => "#E7BF3D",
+        "author_name" => $user->data->email,
+        "author_link" => "mailto:{$user->data->email}",
+        "image_url" => @$_SESSION['user_picture'],
+        "footer" => $user->data->address,
+    ];
+    $client = new Maknz\Slack\Client($url);
+    $client->attach($msg)->send("");
+}
+
+/**
+ * Sends formatted message to Slack.
+ */
+function _sendSlackOnNewApp($app){
+    $title = "Nowe zgłoszenie {$app->number} ({$app->address->city})";
+
+    logger($title, true);
+
+    $url = ('%HOST%' != 'uprzejmiedonosze.net')?
+        'https://hooks.slack.com/services/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': # staging dev
+        'https://hooks.slack.com/services/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'; # prod
+
+    $msg = [
+        "fallback" => $title,
+        "title" => "Nowe zgłoszenie {$app->number}",
+        "title_link" => "%HTTPS%://%HOST%/ud-{$app->id}.html",
+        
+        "color" => "#229A7F",
+
+        "author_name" => "{$app->user->name}",
+        "author_icon" => @$_SESSION['user_picture'],
+        "author_link" => "mailto:{$app->user->email}",
+
+        'fields' => [[
+                'title' => $app->address->city,
+                'value' => ($app->category == 0)? 'Inne: ' . $app->userComment: $app->getCategory()[0],
+                'short' => false
+            ]],
+
+        "image_url" => "%HTTPS%://%HOST%/{$app->contextImage->url}",
+        "thumb_url" => "%HTTPS%://%HOST%/{$app->contextImage->thumb}",
+
+        "footer" => $app->getCategory()[0],
+        "footer_icon" => "%HTTPS%://%HOST%/img/{$app->category}.jpg",
+        "ts" => strtotime($app->date)
+    ];
+    $client = new Maknz\Slack\Client($url);
+    @$client->from($app->user->name)->withIcon($_SESSION['user_picture'])->attach($msg)->send("");
+}
+
+
+/** 
+ * Sends message to #errors slack channel at uprzejmiedonosze.slack.com
+ */
+function _sendSlackError($msg){
+    $url = ('%HOST%' != 'uprzejmiedonosze.net')?
+        'https://hooks.slack.com/services/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': # staging dev
+        'https://hooks.slack.com/services/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'; # prod
+
+    $client = new Maknz\Slack\Client($url);
+    $client->send($msg);
+}
+
+?>
+
