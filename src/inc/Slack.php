e2f1109be6f776654b54403a54fa16de7bd5768f
Szymon Nieradka <szymon[]nieradka.net>
2019-04-28 21:41:02 +0200

    jedna koleja na wszystkie środowiska

--- src/inc/Slack.php
+++ src/inc/Slack.php
@@ -6,16 +6,16 @@ $queue = msg_get_queue(
 );
 
 $channels = [
-    'uprzejmiedonosze.net-1' => 'https://hooks.slack.com/services/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',
-    'uprzejmiedonosze.net-2' => 'https://hooks.slack.com/services/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',
-    'staging.uprzejmiedonosze.net-1' => 'https://hooks.slack.com/services/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',
-    'staging.uprzejmiedonosze.net-2' => 'https://hooks.slack.com/services/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'
+    1 => 'https://hooks.slack.com/services/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', // prod
+    2 => 'https://hooks.slack.com/services/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', // prod
+    11 => 'https://hooks.slack.com/services/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',// staging
+    12 => 'https://hooks.slack.com/services/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' // staging
 ];
 
 $msg = NULL;
 $type = NULL;
 while(msg_receive($queue, 0, $type, 2000, $msg)){
-    if($channel = @$channels["%HOST%-$type"]){
+    if($channel = @$channels[intval($type)]){
         $client = new Maknz\Slack\Client($channel);
         if(is_array($msg)){
             $client->attach($msg)->send("");
