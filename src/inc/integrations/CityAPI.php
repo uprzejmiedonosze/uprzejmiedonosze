f4ed0be4c68b3d5a3e51b91ce12d25ee0370eb39
Szymon Nieradka <szymon[]nieradka.net>
2019-08-03 18:24:43 +0200

    CityAPI::curlSend

--- src/inc/integrations/CityAPI.php
+++ src/inc/integrations/CityAPI.php
@@ -19,6 +19,41 @@ abstract class CityAPI {
             'app' => $application
         ]), 0, $limit);
     }
+
+    function curlSend($url, $auth, $data, $application){
+        $curl = curl_init();
+        $root = realpath('/var/www/%HOST%/');
+
+        $postFields = array(
+            'json' => json_encode($data),
+            'uz_file' => [
+                new CURLFile("$root/" . $application->contextImage->url, "image/jpg", $application->getAppImageFilenamePrefix() . '-kontekst.jpg'),
+                new CURLFile("$root/" . $application->carImage->url, "image/jpg", $application->getAppImageFilenamePrefix() . '-tablica.jpg')
+            ]);
+
+        print_r($postFields);
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => http_build_query($postFields),
+            CURLOPT_HTTPHEADER => array($auth),
+            CURLOPT_SAFE_UPLOAD => true
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            echo $response;
+        }
+    }
 }
 
 require(__DIR__ . '/Poznan.php');
