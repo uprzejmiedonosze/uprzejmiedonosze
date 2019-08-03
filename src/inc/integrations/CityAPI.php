d8243e16d93e780848b4847837c0fd8194019b41
Szymon Nieradka <szymon[]nieradka.net>
2019-08-03 17:41:07 +0200

    generowanie tekstu zgłoszenia do API twigiem

--- src/inc/integrations/CityAPI.php
+++ src/inc/integrations/CityAPI.php
@@ -15,17 +15,9 @@ abstract class CityAPI {
     }
 
     function formatMessage($application, $limit = 10000){
-        return substr(
-            "W dniu {$application->getDate()} {$application->getDateTimeDivider()} {$application->getTime()} " . 
-            "{$application->guessUserSex()['bylam']} świadkiem pozostawienia pojazdu o nr rejestracyjnym " . 
-            "{$application->carInfo->plateId} pod adresem {$application->address->address}. " . 
-            "{$application->getCategory()[1]} Sytuacja jest widoczna na załączonych zdjęciach. " . 
-            "Zdjęcia {$application->guessUserSex()['wykonalam']} samodzielnie. {$application->getJSONSafeComment()}\n\n" . 
-            ((!$application->statements || !$application->statements->witness)? "Nie {$application->guessUserSex()['bylam']} świadkiem samego momentu parkowania. " : " " ) .
-            "Jestem {$application->guessUserSex()['swiadoma']} odpowiedzialności karnej z art. 233 §1–§3 Kodeksu karnego " .
-            "oraz treści art. 182, 183 i 185 Kodeksu Postępowania Karnego.\n\n" .
-            ((!$application->user->exposeData)? "Równocześnie proszę o niezamieszczanie w protokole danych dotyczących mojego miejsca zamieszkania.": "") .
-            "\n\nAdres URL zawierający szczegóły zgłoszenia: %HTTPS%://%HOST%/ud-{$application->id}.html", 0, $limit);
+        return substr(generate('_application.txt.twig', [
+            'app' => $application
+        ]), 0, $limit);
     }
 }
 
