02b49bd5f7c60249e2620d16c505a73fa694e3c1
Szymon Nieradka <szymon[]nieradka.net>
2019-08-13 20:06:47 +0200

    zmieniam status od razu na potwierdzone przez SM

--- src/inc/integrations/Poznan.php
+++ src/inc/integrations/Poznan.php
@@ -26,7 +26,7 @@ class Poznan extends CityAPI {
 
         $reply = "{$output['response']['msg']} (instancja: {$output['response']['instance']}, id: {$output['response']['id']})";
 
-        $application->setStatus('confirmed-waiting');
+        $application->setStatus('confirmed-sm');
         $application->addComment($application->guessSMData()->address[0], $reply);
         $application->sentViaAPI = $reply;
         global $storage;
