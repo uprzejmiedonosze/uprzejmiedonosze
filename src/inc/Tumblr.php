163933c77f93a3c7f504a58ed622959a659605bf
Szymon Nieradka <szymon[]nieradka.net>
2019-12-28 00:20:43 +0100

    Publikuje się do Tumblera!

--- src/inc/Tumblr.php
+++ src/inc/Tumblr.php
@@ -1,7 +1,7 @@
 <?PHP
-require(__DIR__ . '/../../vendor/autoload.php');
+require_once(__DIR__ . '/../vendor/autoload.php');
 
-function addToTumblr($app){
+function addToTumblr(&$app){
     $client = new Tumblr\API\Client(
         'lc2iFjN4b63gjxGrBvfbUCbIjDYkr2ofvpOrOb83oMuBRfETsP',
         'ViFzg3ewByVMvO0hyOIu3ZgBC8nVma1C0WwqoldhOCTDWVYcYL',
@@ -11,26 +11,23 @@ function addToTumblr($app){
     $blogName = 'uprzejmie-donosze';
 
     $recydywa = '';
-    if($app->getRecydywa > 1){
+    if($app->getRecydywa() > 1){
         $recydywa = ' (recydywista)';
     }
 
     $data = array(
         'type' => 'photo', 
-        'caption' => "**{$app->carInfo->plateId}**$recydywa, {$app->address->city}\n\n{$app->getCategory()->getTitle()}\n\n![mapa]({$app->getMapImage()})",
-        'source' => $app->contextImage->url,
+        'caption' => "**{$app->carInfo->plateId}**$recydywa, {$app->address->city}\n\n{$app->getCategory()->getTitle()}\n\n![mapa](%HTTPS%://%HOST%/{$app->getMapImage()})",
+        'source' => "%HTTPS%://%HOST%/{$app->contextImage->url}",
         'format' => 'markdown',
         'tags' => "{$app->address->city}, {$app->carInfo->plateId}",
         'state' => 'queue',
         'date' => $app->date
         );
         
-    try {
-        print_r($client->createPost($blogName, $data));
-    } catch (Exception $ex) {
-        print_r($ex);
-    }
+
+    $reply = $client->createPost($blogName, $data);
+    return "https://uprzejmie-donosze.tumblr.com/#" . @$reply->id;
 }
- 
 
 ?>
