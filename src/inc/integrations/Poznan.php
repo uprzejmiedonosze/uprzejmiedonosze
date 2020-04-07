5a47bfafa0d47c8d108d78097992f3e2ba85d91f
Szymon Nieradka <szymon[]nieradka.net>
2020-04-07 20:14:05 +0200

    poprawka ; na , w Poznań SM API

--- src/inc/integrations/Poznan.php
+++ src/inc/integrations/Poznan.php
@@ -25,7 +25,11 @@ class Poznan extends CityAPI {
             'surname' => $application->getLastName(), //nazwisko zgłaszającego, pole obowiązkowe do 128 znaków
             'email' => $application->user->email, //email użytkownika, pole obowiązkowe
             'subject' => $application->getTitle(), //temat zgłoszenia, pole obowiązkowe do 256 znaków
-            'text' => trim(preg_replace('/\s+/', ' ', parent::formatMessage($application, 4000))),
+            'text' => trim(
+                preg_replace('/\s+/', ' ',
+                    preg_replace('/;/', ',', parent::formatMessage($application, 4000))
+                )
+            ),
             'address' => $application->address->address, //adres, pole opcjonalne, do 256 znaków
             'key' => '85951ba0a63d1051a09659ea0a9d8391' //klucz aplikacji, pole obowiązkowe
         );
