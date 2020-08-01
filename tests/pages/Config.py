934f2e4cc0c3f18a09d9ae3b658f7f429318378a
Szymon Nieradka <szymon[]nieradka.net>
2020-08-01 20:01:20 +0200

    usuwam testy selenium

--- tests/pages/Config.py
+++
@@ -1,8 +0,0 @@
-import configparser
-
-class Config(object):
-    config = configparser.ConfigParser()
-    def __init__(self, file='./config.ini'):
-        self.config.read(file)
-        self.account = self.config['account']
-        self.app = self.config['app']
