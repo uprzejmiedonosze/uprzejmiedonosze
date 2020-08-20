aed8e817a6d98c7dc463036506a4963b02f188c0
Szymon Nieradka <szymon[]nieradka.net>
2020-08-20 21:55:40 +0100

    Dokładniejsza walidacja pola z adresem

--- src/js/script.js
+++ src/js/script.js
@@ -232,7 +232,7 @@ function checkAddress(where) {
     // checking this only on new-application page (not on registration where adddress field name differs)
     if (where.selector == '#lokalizacja') {
         ret = ($('#locality').val().trim().length > 2) && ret;
-        ret = ($('#latlng').val().trim().length > 5) && ret;
+        ret = !!($('#latlng').val().trim().match(/\d\d\.\d+,\d\d\.\d+/)) && ret;
 
         if (!ret && where.val().trim().length > 0) {
             $('#addressHint').text('Podaj adres lub wskaż go na mapie. Ew. uwagi dotyczące lokalizacji napisz w polu komentarz poniżej');
