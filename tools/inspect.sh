00f173baa190e775f8e6681b3fb8c6f0db0d6056
Szymon Nieradka <szymon[]nieradka.net>
2020-10-14 13:54:29 +0200

    lizanie basha

--- tools/inspect.sh
+++ tools/inspect.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
 SQL="sqlite3 /var/www/uprzejmiedonosze.net/db/store.sqlite"
-PARAM=$1
+PARAM="$@"
 
-function get_app_url() {
-	echo -n "      https://uprzejmiedonosze.net/ud-"
-	if [[ $1 =~ ^[0-9a-f-]{36}$ || $1 =~ ^[0-9a-zA-Z]{12}$ ]]; then
-		echo "$1.html"
-	else
-		ssh nieradka.net "${SQL} \"select key || '.html' from applications where json_extract(value, '$.number') = '${PARAM}'\""
-	fi
+function print_app_url() {
+	echo "      https://uprzejmiedonosze.net/ud-$1.html $2"
 }
 
-if [[ ${PARAM} =~ ^[0-9a-f-]{36}$ || $PARAM =~ ^[0-9a-zA-Z]{12}$ ]]; then
-	echo "Checking application id ${PARAM}";
-	ssh nieradka.net "${SQL} \"select value from applications where key = '${PARAM}'\"" | jq '.'
-	get_app_url ${PARAM}
-	exit 0
-fi
+function is_app_key() {
+	[[ "$1" =~ ^[0-9a-f-]{36}$ || "$1" =~ ^[0-9a-zA-Z]{12}$ ]]
+}
 
-if [[ ${PARAM} =~ ^UD/[0-9]+/[0-9]+$ ]]; then
-	echo "Checking appId ${PARAM}";
-	ssh nieradka.net "${SQL} \"select value from applications where json_extract(value, '$.number') = '${PARAM}' \"" | jq '.'
-	get_app_url ${PARAM}
+function is_appId() {
+	[[ "$1" =~ ^[Uu][Dd]/[0-9]+/[0-9]+$ ]]
+}
+
+function get_app_json() {
+	ssh nieradka.net "${SQL} \"select value from applications where key = '$1' \""
+}
+
+function get_app_key_by_id() {
+	ssh nieradka.net "${SQL} \"select key from applications where json_extract(value, '$.number') = upper('$1') \""
+}
+
+is_app_key "$1" && APPKEY=$1
+is_appId "$1" && APPKEY=$(get_app_key_by_id "$1")
+
+if [ ${APPKEY+x} ]; then
+	echo "Checking application id ${APPKEY}";
+	ssh nieradka.net "${SQL} \"select value from applications where key = '${APPKEY}'\"" | jq '.'
+	print_app_url "${APPKEY}"
 	exit 0
 fi
 
@@ -39,8 +46,8 @@ ssh nieradka.net "${SQL} \"select value from users where ${WHERE1} \"" | jq 'del
 echo "Stats:"
 ssh nieradka.net "${SQL} \"select value from applications where ${WHERE2} \"" | jq -r '.status' | sort | uniq -c | sort -nr
 echo "Last 10 applications:"
-for key in $(ssh nieradka.net "${SQL} \"select key from applications where ${WHERE2} order by json_extract(value, '$.added') desc limit 10 \""); do
-	get_app_url $key
+for key in $(ssh nieradka.net "${SQL} \"select key, json_extract(value, '$.status') from applications where ${WHERE2} order by json_extract(value, '$.added') desc limit 10 \""); do
+	print_app_url ${key%|*} "[${key#*|}]"
 done
 exit 0
 
