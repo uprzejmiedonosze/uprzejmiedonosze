7bed85d201cc6a1abaebf329131673485ca8e113
Szymon Nieradka <szymon@nieradka.net>
2021-05-13 22:59:08 +0100

    Moderacja galerii level pro

--- src/js/sites/adm-gallery.js
+++ src/js/sites/adm-gallery.js
@@ -1,22 +1,42 @@
-function _moderateApp(appId, decision) {
-  $.post("/api/api.html", {
-    action: "moderateApp",
-    id: appId,
-    decision: decision
-  })
-    .done(function () {
-      $("#" + appId)
-        .removeClass("decisiontrue decisionfalse")
-        .addClass("decision" + decision);
-    })
-    .fail(function (e) {
-      $.mobile.loading("show", {
-        text: e.statusText,
-        textVisible: true,
-        textonly: true
-      });
-      return false;
-    });
-}
+$(document).on("pageshow", function () {
+  if (!$(".galleryAdmin").length) return;
 
-window._moderateApp = _moderateApp;
+  function scrollNext() {
+    const next = $('.galleryItem:not(.decision)')[0]
+    $([document.documentElement, document.body]).animate({
+      scrollTop: next.offsetTop - 70
+    })
+    $(next).addClass('next')
+  }
+
+  scrollNext()
+
+  document.addEventListener('keypress', ({code}) => {
+    const appId = $('.galleryItem.next').attr('id')
+    if (code === 'KeyD') {
+      window._moderateApp(appId, true)
+    }
+    if (code === 'Escape') {
+      window._moderateApp(appId, false)
+    }
+  });
+
+  window._moderateApp = (appId, decision) => {
+    $("#" + appId).removeClass('next')
+    $("#" + appId).addClass("decision")
+    scrollNext()
+    $.post("/api/api.html", {
+      action: "moderateApp",
+      id: appId,
+      decision: decision
+    }).done(() => {})
+      .fail((e) => {
+        $.mobile.loading("show", {
+          text: e.statusText,
+          textVisible: true,
+          textonly: true
+        });
+        return false;
+      });
+  }
+})
