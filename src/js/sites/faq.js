fbcede2db033a6446cdd1452bc1e357128620344
Szymon Nieradka <szymon@nieradka.net>
2022-04-10 18:21:04 +0200

    direct links to FAQ sections

--- src/js/sites/faq.js
+++ src/js/sites/faq.js
@@ -5,5 +5,20 @@ $(function () {
     $("html, body").animate({
       scrollTop: $(hash).offset().top - 60
     });
+    $('h4'+hash).addClass('highlight')
   }
+  $('h4').append(' <a class="copyLink" data-toggle="tooltip" title="Skopiuk link do sekcji">(link)</a>')
+
+  $('a.copyLink').click(function (e) {
+    e.preventDefault();
+    var copyText = `${window.location.origin}${window.location.pathname}#` + $(this).parent().attr('id');
+
+    document.addEventListener('copy', function(e) {
+       e.clipboardData.setData('text/plain', copyText);
+       e.preventDefault();
+    }, true);
+
+    document.execCommand('copy');
+  });
+  
 });
