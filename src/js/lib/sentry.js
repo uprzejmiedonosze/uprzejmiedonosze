1e1c04331e48899c102fabd496dc7ef98f70c156
Szymon Nieradka <szymon@nieradka.net>
2023-04-05 14:07:15 +0200

    sentry cleanup

--- src/js/lib/sentry.js
+++ src/js/lib/sentry.js
@@ -1,9 +1,7 @@
 import * as Sentry from "@sentry/browser"
-// import { BrowserTracing } from "@sentry/tracing"
 
 Sentry.init({
   dsn: "https://66b5f54d37b1474daf0d95059e69c0b2@o929176.ingest.sentry.io/5878019",
-  // integrations: [new BrowserTracing()],
   tracesSampleRate: 0.3,
   environment: window.location.hostname,
   denyUrls: [
