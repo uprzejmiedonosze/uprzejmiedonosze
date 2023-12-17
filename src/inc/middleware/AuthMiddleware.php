0fed700526a186197a8eeb5ecd32183733a553e7
Szymon Nieradka <szymon@nieradka.net>
2023-12-17 21:35:05 +0000

    api fix getPublicKeys

--- src/inc/middleware/AuthMiddleware.php
+++ src/inc/middleware/AuthMiddleware.php
@@ -99,7 +99,7 @@ class AuthMiddleware implements MiddlewareInterface {
         if (!$publicKeys)
             throw new HttpInternalServerErrorException($request, 'Failed to fetch JWT public keys.');
 
-        $cacheControl = $request->getHeader('Cache-Control');
+        $cacheControl = $this->parseHeaders($http_response_header)['Cache-Control'];
         preg_match('/max-age=(\d+)/', $cacheControl, $out);
         $timeout = $out[1];
         $cache->set($cacheKey, $publicKeys, 0, (int)$timeout);
@@ -107,6 +107,22 @@ class AuthMiddleware implements MiddlewareInterface {
         return json_decode($publicKeys, true);
     }
 
+    private /**
+    * @SuppressWarnings(PHPMD.UnusedLocalVariable)
+    */
+   function parseHeaders($headers) {
+       $head = array();
+       foreach ($headers as $k => $v) {
+           $header = explode(':', $v, 2);
+           if (isset($header[1])) {
+               $head[trim($header[0])] = trim($header[1]);
+               continue;
+           }
+           $head[] = $v;
+           if (preg_match("#HTTP/[0-9\.]+\s+([0-9]+)#", $v, $out))
+               $head['reponse_code'] = intval($out[1]);
+       }
+       return $head;
+   }
+
 }
-
-
