5fafe3856f950987fd506388eaa4b8e0f729b79a
SZN <szymon@nieradka.net>
2023-12-24 13:31:31 +0000

    API: missing param 400 status code

--- src/inc/middleware/ApiUtils.php
+++ src/inc/middleware/ApiUtils.php
@@ -1,10 +1,9 @@
 <?PHP
 
-
 function getParam($params, $name, $default=null) {
     $param = $params[$name] ?? $default;
     if(is_null($param)) {
-        throw new Exception("Missing required parameter '$name'");
+        throw new Exception("Missing required parameter '$name'", 400);
     }
     return $param;
 }
