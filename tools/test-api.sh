efb12fd34273f868bd08602965dacb86e35ede28
SZN <szymon@nieradka.net>
2023-12-25 17:55:49 +0100

    API: test-epi supports staging

--- tools/test-api.sh
+++ tools/test-api.sh
@@ -1,34 +1,79 @@
 #!/bin/bash                                                             
 set -euo pipefail
 
-CURL="curl -s"
-JWT=sample.jwt.token
-AUTH="Authorization: Bearer $JWT"
-HOST="http://localhost:8080"
 GREEN="\033[00;32m"
 RED="\033[0;31m"
 BLUE="\033[0;35m"
 LIGHT_GRAY="\033[1;30m"
 RESET_COLOR="\033[m"
 
-DEBUG="$*"
+function usage() { echo -e "$*\nUsage: $(basename $0) [-v] [-t jwt]"; }
+function tGroup() { echo -e "\n$BLUE$*$RESET_COLOR"; }
+function Test() { echo -n "  $*"; }
+function log() { echo -e "$LIGHT_GRAY$*$RESET_COLOR"; }
 
-DB="docker/db/store.sqlite"
-cp $DB $DB~
+JWT="eyJhbGciOiJSUzI1NiIsImtpZCI6IjUyNmM2YTg0YWMwNjcwMDVjZTM0Y2VmZjliM2EyZTA4ZTBkZDliY2MiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiU3p5bW9uIE5pZXJhZGthIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FMbTV3dTFXQ0plUjQzOEd3NnQtUzVUMktXaEJuNkp1V2plSlF2d0VqT2RDTnFjPXM5Ni1jIiwiaXNzIjoiaHR0cHM6Ly9zZWN1cmV0b2tlbi5nb29nbGUuY29tL3Vwcnplam1pZWRvbm9zemUtMTQ5NDYwNzcwMTgyNyIsImF1ZCI6InVwcnplam1pZWRvbm9zemUtMTQ5NDYwNzcwMTgyNyIsImF1dGhfdGltZSI6MTcwMzEwNTIwOCwidXNlcl9pZCI6IjBTemJsNlhmSDhPdDVNTmZjRkVxYllpYUtYWDIiLCJzdWIiOiIwU3pibDZYZkg4T3Q1TU5mY0ZFcWJZaWFLWFgyIiwiaWF0IjoxNzAzNTE5Nzk0LCJleHAiOjE3MDM1MjMzOTQsImVtYWlsIjoic3p5bW9uQG5pZXJhZGthLm5ldCIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7Imdvb2dsZS5jb20iOlsiMTAzNDkxODk0MTUwMzIxMTU0NjE5Il0sImVtYWlsIjpbInN6eW1vbkBuaWVyYWRrYS5uZXQiXX0sInNpZ25faW5fcHJvdmlkZXIiOiJnb29nbGUuY29tIn19.SpXqpopC6dXiM0nXuUtKGEyTlxm3aCTok_FSHfockgKdVJOcBURZEtlD26I42rBXACEt6FhOlYi0UlwDnqakNjAMQwc5AoY_87FYEHhGc9D-x0jIsfjjqOyPoayd1bqtgoOPEQDQXTfS1fL1Ycx61Ac_RNgjsjxrL_pl3U3lEQwIumLpOccu2-sqZE0JNDlBkwAPzShx0ITgqfrCxHV0ZLmv5RpGnj7-6zfiw9PwbYRN48GvIOmctcQErmX7DTOmWrmFEU0pqPFO-iFE7rNODfPNFFQA4DhiR9Gaf8Qyydp6Vwy-gDMu3NGxtShX5qntRIgmA8Er28fK6bosTdgYPg"
+ENV=local
+VERBOSE=0
+CURL="curl -s"
 
-MEMCACHED=$(curl localhost:11211 2>&1 | grep -c Fail || true)
-test "$MEMCACHED" -eq 1 && (echo "Starting memcached"; memcached &)
 
-trap "mv $DB~ $DB; echo -e '$RESET_COLOR'; test "$MEMCACHED" -eq 1 && killall memcached; exit" INT TERM EXIT
+while getopts ':vt:h' opt; do
+  case "$opt" in
+    v)
+      VERBOSE=1
+      ;;
+    t)
+      JWT="$OPTARG"
+      ENV=staging
+      ;;
+    h)
+      usage; exit 0
+      ;;
+    :)
+      usage option requires an argument.; exit 1
+      ;;
+    ?)
+      usage Invalid command option.; exit 1
+      ;;
+  esac
+done
+shift "$((OPTIND -1))"
 
-function PASS() {
-    echo -e "$GREEN"pass"$RESET_COLOR"
+
+decode_jwt() {
+    local payload
+    payload=$(echo -n "$1" | cut -d "." -f 2)
+    local len=$((${#payload} % 4))
+    if [ $len -eq 2 ]; then payload="$1"'=='
+    elif [ $len -eq 3 ]; then payload="$1"'=' 
+    fi
+    echo "$payload" | tr '_-' '/+' | openssl enc -d -base64 | jq -r .email
 }
+
+HOST="localhost:8080"
+EMAIL=$(decode_jwt "$JWT")
+
+if [ $ENV = 'staging' ]; then
+    log Usign staging API with given JWT token
+    ssh nieradka.net "cd /var/www/staging.uprzejmiedonosze.net/db && cp store.sqlite-empty store.sqlite"
+    HOST="https://apistaging.uprzejmiedonosze.net"
+else
+    log Usign localhost API, local DB and memcache
+    DB="docker/db/store.sqlite"
+    cp $DB $DB~
+    MEMCACHED=$(curl localhost:11211 2>&1 | grep -c Fail || true)
+    test "$MEMCACHED" -eq 1 && (log "Starting memcached"; memcached &)
+    trap 'log Reverting DB and killing memcache; mv $DB~ $DB; echo -e "$RESET_COLOR"; test "$MEMCACHED" -eq 1 && killall memcached; exit' INT TERM EXIT
+fi
+
+AUTH="Authorization: Bearer $JWT"
+
+function PASS() { echo -e "$GREEN"pass"$RESET_COLOR"; }
 
 function FAIL() {
     echo -e "${RED}fail, got $1$RESET_COLOR"
-    test "$DEBUG" = "-d" && (echo -e "\n$LIGHT_GRAY$2$RESET_COLOR"; exit 1)
-    
+    test $VERBOSE -eq 1 && (echo -e "\n$LIGHT_GRAY$2$RESET_COLOR"; exit 1)
 }
 
 function testStatus() {
@@ -46,21 +91,12 @@ function testOutput() {
     test "$OUTPUT" = "$4" && PASS || FAIL "$OUTPUT" "$RAW"
 }
 
-function getFirstDraft() {
+function getUserNumber() {
+    $CURL $HOST/user -H "$AUTH" | jq -r '.number'
+}
+function getFirstDraftId() {
     $CURL $HOST/user/apps?status=draft -H "$AUTH" | jq -r '.[].id'
 }
-
-function tGroup() {
-    echo -e "\n$BLUE$*$RESET_COLOR"
-}
-
-function Test() {
-    echo -n "  $*"
-}
-
-
-
-# reset DB
 
 tGroup Testing static routes
 
@@ -97,7 +133,8 @@ testStatus PATCH "/user/confirm-terms" 404 "$AUTH"
 tGroup Testing scenario new user scenario
 
 Test Create user
-testOutput PATCH "/user" .data.email "test@user" "$AUTH"
+testOutput PATCH "/user" .data.email "$EMAIL" "$AUTH"
+USER_NUMBER=$(getUserNumber)
 Test Create again
 testOutput PATCH "/user" .isRegistered "false" "$AUTH"
 Test Get user
@@ -122,16 +159,12 @@ Test Create draft
 testOutput POST "/app/new" .status draft "$AUTH"
 Test Get drafts
 testOutput GET "/user/apps?status=draft" length 1 "$AUTH"
-APP_ID=$(getFirstDraft)
+APP_ID=$(getFirstDraftId)
 Test Failed change status
 testOutput PATCH "/app/$APP_ID/status/confirmed" .reason "Odmawiam zmiany statusu z draft na confirmed dla zgłoszenia $APP_ID" "$AUTH"
 Test Failed send
 testOutput PATCH "/app/$APP_ID/send" .error "Nie mogę wysłać zgłoszenia w statusie draft" "$AUTH"
 Test Add image
-testOutput POST "/app/$APP_ID/image" .carImage.url "cdn2/3/$APP_ID,ca.jpg" "$AUTH" \
+testOutput POST "/app/$APP_ID/image" .carImage.url "cdn2/$USER_NUMBER/$APP_ID,ca.jpg" "$AUTH" \
     '--form 'image=@"/Users/szn/dev/webapp/cypress/fixtures/img_p.jpg"' --form 'pictureType="carImage"''
-
-# post /app/{appId}
-# patch /app/{appId}/status/{status}
-# post /app/{appId}/image
 
