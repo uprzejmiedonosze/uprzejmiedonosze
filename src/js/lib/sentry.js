bc6a3a808d20ba48bf5d75bfec1639eb8fc7f308
SZN <szymon@nieradka.net>
2024-01-03 17:49:38 +0100

    disable sentry on staging

--- src/js/lib/sentry.js
+++ src/js/lib/sentry.js
@@ -1,27 +1,30 @@
 import * as Sentry from "@sentry/browser"
 
-Sentry.init({
-  dsn: "https://66b5f54d37b1474daf0d95059e69c0b2@o929176.ingest.sentry.io/5878019",
-  tracesSampleRate: 0.3,
-  environment: window.location.hostname,
-  denyUrls: [
-    // Google Adsense
-    /pagead\/js/i,
-    // Facebook flakiness
-    /graph\.facebook\.com/i,
-    // Facebook blocked
-    /connect\.facebook\.net\/en_US\/all\.js/i,
-    // Woopra flakiness
-    /eatdifferent\.com\.woopra-ns\.com/i,
-    /static\.woopra\.com\/js\/woopra\.js/i,
-    // Chrome extensions
-    /extensions\//i,
-    /^chrome:\/\//i,
-    // Other plugins
-    /127\.0\.0\.1:4001\/isrunning/i,  // Cacaoweb
-    /webappstoolbarba\.texthelp\.com\//i,
-    /metrics\.itunes\.apple\.com\.edgesuite\.net\//i
-  ]
-});
+if (!window.location.hostname.includes('staging')) {
+  Sentry.init({
+    dsn: "https://66b5f54d37b1474daf0d95059e69c0b2@o929176.ingest.sentry.io/5878019",
+    tracesSampleRate: 0.3,
+    environment: window.location.hostname,
+    denyUrls: [
+      // Google Adsense
+      /pagead\/js/i,
+      // Facebook flakiness
+      /graph\.facebook\.com/i,
+      // Facebook blocked
+      /connect\.facebook\.net\/en_US\/all\.js/i,
+      // Woopra flakiness
+      /eatdifferent\.com\.woopra-ns\.com/i,
+      /static\.woopra\.com\/js\/woopra\.js/i,
+      // Chrome extensions
+      /extensions\//i,
+      /^chrome:\/\//i,
+      // Other plugins
+      /127\.0\.0\.1:4001\/isrunning/i,  // Cacaoweb
+      /webappstoolbarba\.texthelp\.com\//i,
+      /metrics\.itunes\.apple\.com\.edgesuite\.net\//i
+    ]
+  });
 
-Sentry.setTag("environment", window.location.hostname);
+  Sentry.setTag("environment", window.location.hostname);
+
+}
