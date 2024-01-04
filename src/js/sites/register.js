154e14b736adf683ad275eaf283138b3a9261946
SZN <szymon@nieradka.net>
2024-01-04 17:37:34 +0100

    Register form copy, validation and autocomplete removal

--- src/js/sites/register.js
+++ src/js/sites/register.js
@@ -1,8 +1,11 @@
-import { checkValueRe } from "../lib/validation";
+import { checkValueRe } from "../lib/validation"
+
+const $name = $("#name")
+const $address = $("#address")
 
 function validateRegisterForm() {
-  ret = checkValueRe($("#name"), /^(\S{2,5}\s)?\S{3,20}\s[\S -]{3,40}$/i)
-  ret = checkValueRe($("#address"), /^.{3,50}\d.{3,40}$/i) && ret
+  ret = checkValueRe($name, /^(\S{2,5}\s)?\S{3,20}\s[\S -]{3,40}$/i)
+  ret = checkValueRe($address, /^.{3,50}\d.{3,40}\D$/i) && ret
   if (!ret) {
     $(window).scrollTop($(".error").offset().top - 100);
   }
@@ -12,14 +15,12 @@ function validateRegisterForm() {
 $(document).on("pageshow", function () {
   if (!$(".register").length) return;
 
-  initAutocompleteRegister();
-
-  $("#name").on("change", function () {
-    $("#name").removeClass("error");
+  $name.on("change", function () {
+    $name.removeClass("error");
   });
 
-  $("#address").on("change", function () {
-    $("#address").removeClass("error");
+  $address.on("change", function () {
+    $address.removeClass("error");
   });
 
   $("#register-submit").click(function () {
