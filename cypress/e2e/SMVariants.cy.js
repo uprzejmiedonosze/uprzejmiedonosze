b46dcbb2d716c93471e61e27721e3224c0375f64
SZN <szymon@nieradka.net>
2024-01-16 11:04:26 +0100

    cypress

--- cypress/e2e/SMVariants.cy.js
+++ cypress/e2e/SMVariants.cy.js
@@ -139,12 +139,11 @@ describe('Missing SM (Poniatowa)', () => {
 
     it('creates application', function () {
         cy.goToNewAppScreen()
-        cy.uploadOKImages()
-        cy.setAppCategory(this.categories)
-        cy.get('#lokalizacja').clear().type('Henin 93, Poniatowa')
-        cy.wait(1000)
-        cy.get('.pac-container .pac-item', { timeout: 5000 }).first().click()
+        cy.uploadOKImages('poniatowa.jpg')
+        cy.wait(500)
         cy.get('#geo').should('have.class', 'ui-icon-location')
+        cy.setAppCategory(this.categories)
+        cy.wait(500)
         cy.get('#form-submit', { timeout: 5000 }).click()
         cy.contains('Zapisz!').click()
         cy.contains('Wystąpił błąd').should('not.exist')
