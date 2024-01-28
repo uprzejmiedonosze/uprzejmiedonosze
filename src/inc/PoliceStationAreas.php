aac046a7e1d3a68e433ae667be338c94c3679e58
SZN <szymon@nieradka.net>
2024-01-28 19:24:51 +0000

    PoliceStationAreas::pointInPolygon refactor

--- src/inc/PoliceStationAreas.php
+++ src/inc/PoliceStationAreas.php
@@ -15,7 +15,7 @@ class PoliceStationAreas {
     private $polygons = Array();
     
     public function __construct() {
-        $policeStations = file_get_contents(__DIR__ . "/../public/api/config/police-stations.json");
+        $policeStations = file_get_contents(__DIR__ . "/../public/api/config/police-stations.pjson");
         $this->polygons = unserialize($policeStations);
     }
 
@@ -37,17 +37,25 @@ class PoliceStationAreas {
         for ($i=1; $i < $verticesCount; $i++) {
             $vertex1 = $vertices[$i-1];
             $vertex2 = $vertices[$i];
-            if ($vertex1['y'] == $vertex2['y'] && $vertex1['y'] == $point['y'] && $point['x'] > min($vertex1['x'], $vertex2['x']) && $point['x'] < max($vertex1['x'], $vertex2['x'])) { // Check if point is on an horizontal polygon boundary
+            // Check if point is on an horizontal polygon boundary
+            if ($vertex1['y'] == $vertex2['y']
+                && $vertex1['y'] == $point['y']
+                && $point['x'] > min($vertex1['x'], $vertex2['x'])
+                && $point['x'] < max($vertex1['x'], $vertex2['x']))
                 return true; // "boundary";
-            }
-            if ($point['y'] > min($vertex1['y'], $vertex2['y']) && $point['y'] <= max($vertex1['y'], $vertex2['y']) && $point['x'] <= max($vertex1['x'], $vertex2['x']) && $vertex1['y'] != $vertex2['y']) {
+
+            // Check if point is on the polygon boundary (other than horizontal)
+            if ($point['y'] > min($vertex1['y'], $vertex2['y'])
+                && $point['y'] <= max($vertex1['y'], $vertex2['y'])
+                && $point['x'] <= max($vertex1['x'], $vertex2['x'])
+                && $vertex1['y'] != $vertex2['y']) {
                 $xinters = ($point['y'] - $vertex1['y']) * ($vertex2['x'] - $vertex1['x']) / ($vertex2['y'] - $vertex1['y']) + $vertex1['x'];
-                if ($xinters == $point['x']) { // Check if point is on the polygon boundary (other than horizontal)
+                
+                if ($xinters == $point['x'])
                     return true; // "boundary";
-                }
-                if ($vertex1['x'] == $vertex2['x'] || $point['x'] <= $xinters) {
+
+                if ($vertex1['x'] == $vertex2['x'] || $point['x'] <= $xinters)
                     $intersections++;
-                }
             }
         }
         // If the number of edges we passed through is odd, then it's in the polygon.
