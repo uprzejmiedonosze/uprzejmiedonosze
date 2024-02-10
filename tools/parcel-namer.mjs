d1b739fd84df9a06acb5460873f25da7ef421800
SZN <szymon@nieradka.net>
2024-02-10 21:45:40 +0000

    images with subdirs

--- tools/parcel-namer.mjs
+++ tools/parcel-namer.mjs
@@ -5,10 +5,16 @@ export default new Namer({
   name({bundle}) {
     const me = bundle.getMainEntry()
     if (!me) return null;
+    
     const filename = path.parse(me.filePath)
     let ext = filename.ext
     if (bundle.type == 'css')
         ext = '.css'
-    return `${filename.name}${ext}`
+    
+    let subdir = ''
+    const imageSubdir = me.filePath.match(/.*\/src\/img\/(.*)\/.*\..*/)
+    if (imageSubdir)
+      subdir = imageSubdir[1] + '/'
+    return `${subdir}${filename.name}${ext}`
   }
 });
