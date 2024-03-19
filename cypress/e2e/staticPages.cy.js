00d9ee77d6034d6d5ef3101e08e2eecd300f4880
SZN <szymon@nieradka.net>
2024-03-19 18:03:37 +0100

    Cypress: staticPages

--- cypress/e2e/staticPages.cy.js
+++ cypress/e2e/staticPages.cy.js
@@ -1,82 +1,108 @@
-describe('Checks static pages', function() {
+describe('Static pages no session', function() {
     before(() => {
         cy.clearCookie('UDSESSIONID')
+        cy.clearCookie('PHPSESSID')
     })
 
     beforeEach(() => {
         cy.loadConfig()
         cy.visit('/')
-        cy.contains('Menu').click()
     })
 
-    it('Visits main page', () => {
+    it('/', () => {
         cy.contains('Zgłoś nieprawidłowe parkowanie!')
         cy.contains('Suppi')
 
-        cy.contains('– galeria')
-
+        cy.contains('Menu').click()
         cy.contains('Zaloguj')
         cy.contains('zarejestruj')
     })
 
-    it('Visits przepisy', () => {
-        cy.contains('– przepisy').click()
-        cy.contains('Pojazd może być usunięty z drogi na koszt właściciela')
-
+    it('/ » regulamin', () => {
+        cy.contains('regulamin').click()
+        cy.contains('anonimowe dane statystyczne')
     })
 
-    it('Visits faq', () => {
+    it('/ » historia', () => {
+        cy.contains('historia zmian').click()
+        cy.contains('Poniedziałek, 13 lipca 2020')
+    })
+
+    it('/ » o projekcie', () => {
+        cy.contains('o projekcie').click()
+        cy.contains('Bitbucket')
+    })
+
+    it('/menu » Jak zgłaszać', () => {
+        cy.contains('Menu').click()
+        cy.contains('Jak zgłaszać').click()
+        cy.contains('przez Uprzejmie Donoszę')
+    })
+
+    it('/menu » Jak zgłaszać » dzwoń jak szeryf', () => {
+        cy.contains('Menu').click()
+        cy.contains('Jak zgłaszać').click()
+        cy.contains('Stosuj na całych patoparkingach').click()
+        cy.contains('sprawdzaj efekty pracy SM').click()
+        cy.contains('Proszę o przekazanie ww.')
+    })
+
+
+    it('/menu » przepisy', () => {
+        cy.contains('Menu').click()
+        cy.contains('– przepisy').click()
+        cy.contains('Pojazd może być usunięty z drogi na koszt właściciela')
+    })
+
+    it('/menu » faq', () => {
+        cy.contains('Menu').click()
         cy.contains('faq').click()
         cy.contains('Konstantynów Łódzki')
         cy.contains('Szczecin')
     })
 
-    it('Visits regulamin', () => {
-        cy.contains('regulamin').click()
-        cy.contains('anonimowe dane statystyczne')
+    it('/menu » faq » aplikacja', () => {
+        cy.contains('Menu').click()
+        cy.contains('faq').click()
+        cy.contains('wygodny skrót').click()
+        cy.contains('Zainstaluj przeglądarkę')
     })
 
-    it('Visits statystyki', () => {
-        cy.contains('statystyki').click()
+    it('/menu » przesłuchanie', () => {
+        cy.contains('Menu').click()
+        cy.contains('przesłuchanie świadka').click()
+        cy.contains('Czy straż miejska wezwie mnie na przesłuchanie?')
+    })
+
+    it('/menu » przesłuchanie » zwrot', () => {
+        cy.contains('Menu').click()
+        cy.contains('przesłuchanie świadka').click()
+        cy.contains('zwrot środków za przesłuchanie').click()
+        cy.contains('Zwrot utraconego dochodu')
+    })
+
+    it('/menu » Statystyki', () => {
+        cy.contains('Menu').click()
+        cy.contains('Statystyki').click()
         cy.contains('Nowe zgłoszenia oraz nowi użytkownicy')
     })
 
-    it('Visits historia', () => {
-        cy.contains('historia').click()
-        cy.contains('Poniedziałek, 13 lipca 2020')
+})
+
+describe('Static pages logged in', function() {
+    before(() => {
+        cy.clearCookie('UDSESSIONID')
+        cy.clearCookie('PHPSESSID')
     })
 
-    it('Visits o projekcie', () => {
-        cy.contains('o projekcie').click()
-        cy.contains('Bitbucket')
-    })
-
-    it('Visits zwrot za przesłuchanie', () => {
-        cy.visit('/zwrot-za-przesluchanie.html')
-        cy.contains('Jak uzyskać zwrot środków')
-    })
-
-    it('Visits jak zgłosić nielegalne parkowanie', () => {
-        cy.visit('/jak-zglosic-nielegalne-parkowanie.html')
-        cy.contains('przez Uprzejmie Donoszę')
-    })
-
-    it('Visits dzwoń jak szeryf', () => {
-        cy.contains('dzwoń do SM').click()
-        cy.contains('Dzwonisz do Straży Miejskiej')
-        cy.contains('Proszę o przekazanie ww.')
-    })
-
-    it('Visits dzwoń jak szeryf after login', () => {
-        cy.login()
-        cy.contains('dzwoń do SM').click()
-        cy.contains('adres poczty elektronicznej: e@nieradka.net.')
-    })
-
-    it('Visits main page after login', function () {
+    beforeEach(() => {
+        cy.loadConfig()
         cy.login()
         cy.visit('/')
-        cy.contains('Cześć Tester')
+    })
+
+    it('/', function () {
+        cy.contains('Cześć Ud,')
         Object.values(this.levels).forEach(level => {
             cy.contains(level.desc)
         })
@@ -85,6 +111,15 @@ describe('Checks static pages', function() {
         })
         cy.get('.badge').should('not.have.class', 'active')
         cy.contains('wkurzony, ale walczący')
+    })
+
+    it('/menu » Jak zgłaszać » dzwoń jak szeryf', () => {
+        cy.contains('Menu').click()
+        cy.contains('Jak zgłaszać').click()
+        cy.contains('Stosuj na całych patoparkingach').click()
+        cy.contains('sprawdzaj efekty pracy SM').click()
+        cy.contains('Proszę o przekazanie ww.')
+        cy.contains('adres poczty elektronicznej: e@nieradka.net.')
     })
 
 })
