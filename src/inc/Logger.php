9dd41d178cac252c5f2c88a5be5505d64985c4a4
SZN <szymon@nieradka.net>
2024-05-27 13:22:08 +0200

    syslog support

--- src/inc/Logger.php
+++ src/inc/Logger.php
@@ -19,22 +19,28 @@ function isDev() {
 function logger(string|object|array|null $msg, $force = null): string {
     $DT_FORMAT = 'Y-m-d\TH:i:s';
     $time = date($DT_FORMAT);
+    if (is_null($msg))
+        $msg = 'null';
+    if (!is_string($msg))
+        $msg = print_r($msg, true);
+
     if(!isProd() || $force) {
-        $sessionId = session_id() ?? 'no-session';
-
         $user = "[" . ($_SESSION['user_email'] ?? '') . ']';
-
-        if (is_null($msg))
-            $msg = 'null';
-        if (!is_string($msg))
-            $msg = print_r($msg, true);
 
         $debug_backtrace = debug_backtrace();
         $caller = array_shift($debug_backtrace);
         $location = $caller['file'] . ':' . $caller['line'];
         $location = preg_replace('/^.var.www.%HOST%.webapp/i', '', $location);
 
-        error_log("$time $user $sessionId $location\t$msg\n", 3, "/var/log/uprzejmiedonosze.net/%HOST%.log");
+        error_log("$time $user $location\t$msg\n", 3, "/var/log/uprzejmiedonosze.net/%HOST%.log");
     }
+    if(isProd())
+        send_syslog($msg, $force == null);
     return $time;
+}
+
+function send_syslog(string $msg, bool $debug) {
+    openlog("uprzejmiedonosze", LOG_PID | LOG_PERROR, LOG_LOCAL0);
+    syslog($debug ? LOG_DEBUG : LOG_INFO, ($debug ? '[DEBUG]' : '[INFO]') . $msg);
+    closelog();
 }
