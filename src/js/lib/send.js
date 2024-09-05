236b1e4b4b2cf039e8ad6aeada7afc1211cae637
SZN <szymon@nieradka.net>
2024-09-05 20:08:27 +0200

    email send error handling

--- src/js/lib/send.js
+++ src/js/lib/send.js
@@ -8,40 +8,44 @@ import { showMessage, showError } from './showMessage'
 import Api from './Api'
 
 window.sendApplication = async function (appId) {
-  $(`#${appId} .status-confirmed-waiting`).addClass("ui-disabled");
-  $('.ui-btn-right').addClass("ui-disabled");
+  const $whatNext = $(".whatNext")
+  const $afterSend = $(".afterSend")
+  const $buttonRight = $('.ui-btn-right')
 
-  $.mobile.loading("show", { text: "Wysyłam...", textVisible: true });
+  $(`#${appId} .status-confirmed-waiting`).addClass("ui-disabled")
+  $buttonRight.addClass("ui-disabled")
+
+  $.mobile.loading("show", { text: "Wysyłam...", textVisible: true })
 
   try {
     const api = new Api(`/api/app/${appId}/send`)
     const msg = await api.patch()
-    updateStatus(appId, msg.status);
-    $.mobile.loading("hide");
+    updateStatus(appId, msg.status)
+    $.mobile.loading("hide")
     showMessage("<p>Wysłane</p>")
     if ($(".dziekujemy").length) {
-      $(".whatNext").hide();
-      $(".afterSend").show();
+      $whatNext.hide();
+      $afterSend.show();
     }
     if ($('.my-applications').length) {
       $(`#${appId}`).trigger('collapsibleexpand')
     }
-    $('.ui-btn-right').removeClass("ui-disabled");
-    (typeof ga == 'function') && ga("send", "event", { eventCategory: "js", eventAction: "sendViaAPI" });
+    (typeof ga == 'function') && ga("send", "event", { eventCategory: "js", eventAction: "sendViaAPI" })
   } catch (e) {
-    $.mobile.loading("hide");
-    $('.ui-btn-right').removeClass("ui-disabled");
+    $.mobile.loading("hide")
     showError(e.message)
+    $whatNext.hide()
+    $afterSend.text('Błąd: ' + e.message).show().addClass('error')
     Sentry.captureException(e, {
       extra: e.message
-    });
+    })
     if (typeof ga == 'function')
       ga("send", "event", {
         eventCategory: "js-error",
         eventAction: "sendViaAPI"
-      });
+      })
 
   }
-  
+  $buttonRight.removeClass("ui-disabled")
 };
 
