0de899aa6c18b0b5abeca52ea1a32aef2f5a794c
SZN <szymon@nieradka.net>
2024-09-30 09:28:13 +0200

    marketing

--- marketing/mask.sh
+++ marketing/mask.sh
@@ -5,5 +5,5 @@ IFS=$'\n\t'
 MASKA=mask.png
 OUT=$(date +%Y-%m-%d).png
 
-composite -geometry +210+230 \( l.jpg -resize 790x1060 \) ${MASKA} ${OUT}
-composite -geometry +819+230 \( p.jpg -resize 590x1060 \) ${OUT} ${OUT}
+composite -geometry +210+230 \( l.png -resize 790x1060 \) ${MASKA} ${OUT}
+composite -geometry +819+230 \( r.png -resize 590x1060 \) ${OUT} ${OUT}
