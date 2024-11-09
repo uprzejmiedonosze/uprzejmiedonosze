f9d521bf6f25236d8d8675a3eb40f79b5f923320
SZN <szymon@nieradka.net>
2024-11-09 16:32:34 +0100

    how-to refactor

--- src/js/sites/how.js
+++ src/js/sites/how.js
@@ -1,42 +1,15 @@
-function activate(howto, makeActive=true) {
-  const content = howto.getElementsByClassName("content")[0]
-  if (makeActive) {
-    //content.style.maxHeight = content.scrollHeight + "px";
-    howto.classList.add("active")
-    return
-  }
-  //content.style.maxHeight = null
-  howto.classList.remove("active")
-  
+$(function () {
+  scrollTo(window.location.hash)
+})
+
+function scrollTo(hash) {
+  const $hash = $(hash)
+  const scrollTop = (($hash.offset() ?? {}).top ?? 0) - 60
+  if (scrollTop > 0)
+    $("html, body").animate({scrollTop})
+  $('.howto').removeClass('highlight')
+  $hash.addClass('highlight')
 }
 
 // @ts-ignore
-window._activateHowTo = (id) => {
-  const howtos = document.getElementsByClassName("howto") || [];
-  Array.from(howtos).forEach(howto => {
-    activate(howto, id == howto.id)
-  })
-  return false
-}
-
-function handleCardClick() {
-  const howtos = document.getElementsByClassName("howto") || [];
-  const hash = window.location.hash
-
-  Array.from(howtos).forEach(howto => {
-    if (hash == `#${howto.id}`) activate(howto)
-    
-    const links = howto.getElementsByTagName("a") || []
-    Array.from(links).forEach(link =>
-      link.addEventListener("click", 
-        event => event.stopPropagation()))
-
-    howto.addEventListener("click", () => { 
-      const wasActive = howto.classList.contains("active")
-      window.location.hash = wasActive ? "" : howto.id
-      activate(howto, !wasActive)
-    })
-  })
-}
-
-$(handleCardClick);
+window._scrollToHowTo = scrollTo
