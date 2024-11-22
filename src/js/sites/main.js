bdd1f07b1b3e1bbbf9cac90e9848be77abad6ff7
SZN <szymon@nieradka.net>
2024-11-22 09:56:31 +0100

    dialogs

--- src/js/sites/main.js
+++ src/js/sites/main.js
@@ -5,3 +5,22 @@ $(document).on("pageshow", function () {
   // @ts-ignore
   (typeof ga == 'function') && ga("send", "event", { eventCategory: "pageshow", eventAction: "index" });
 });
+
+
+const dialogs = document.querySelectorAll('dialog')
+
+dialogs.forEach(dialog =>
+  dialog.addEventListener('mousedown', event => {
+    if (event.target === event.currentTarget) {
+        event.currentTarget?.close()
+    }
+  })
+)
+
+const popups = document.querySelectorAll('a[data-rel=popup]');
+popups.forEach(popup => {
+  popup.addEventListener("click", function(e) {
+    const id = this.hash.substring(1)
+    document.getElementById(id)?.showModal()
+  })
+})
