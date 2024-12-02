89e651694efeb8d568f99c99f828e391ea2a2423
SZN <szymon@nieradka.net>
2024-12-02 21:28:38 +0100

    filterable runs after load

--- src/js/lib/filterable.js
+++ src/js/lib/filterable.js
@@ -7,17 +7,24 @@ import delay from './delay'
 function filterable(inputId, listId) {
     const input = document.getElementById(inputId)
     if (!input) return
-    input.addEventListener("input", delay(function () {
+    input.addEventListener("input", delay(filter(inputId, listId), 300), false)
+
+    if (input.value.trim())
+        input.dispatchEvent(new Event('input', {bubbles: true}))
+}
+
+function filter(inputId, listId) {
+    return function () {
         // @ts-ignore
-        const filter = this.value.toUpperCase()
+        const filter = this.value.toUpperCase().trim()
         const ul = document.getElementById(listId ?? (inputId + '-list'))
         const lis = ul?.children || []
-    
+
         for(let li of lis) {
             const filtertext = li.dataset.filtertext?.toUpperCase()
             li.style.display = (filtertext?.indexOf(filter) || 0) > -1? "": "none";
         }
-    }, 300), false)
+    }
 }
 
 export default filterable
