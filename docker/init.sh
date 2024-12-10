1f0f0a14dbb03c83eb1b995064118f8e5bcc4701
SZN <szymon@nieradka.net>
2024-12-10 12:25:22 +0000

    docker logs

--- docker/init.sh
+++ docker/init.sh
@@ -2,6 +2,9 @@
 /etc/init.d/memcached start
 /etc/init.d/php8.2-fpm start
 
-xtail -f /var/log/uprzejmiedonosze.net/ &
+
+xtail -f \
+    /var/log/uprzejmiedonosze.net/error.log \
+    /var/log/uprzejmiedonosze.net/uprzejmiedonosze.localhost.log &
 
 exec nginx -g 'daemon off;'
