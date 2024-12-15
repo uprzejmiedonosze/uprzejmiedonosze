9ae62e40cf73caf1bfb006fd480e288a0e374579
SZN <szymon@nieradka.net>
2024-12-15 15:15:48 +0000

    inspect.sh

--- tools/inspect.sh
+++ tools/inspect.sh
@@ -46,7 +46,7 @@ if [[ ${PARAM} =~ @ ]]; then
 	WHERE2="email = '${PARAM}'"
 elif [[ ${PARAM} =~ [0-9] ]]; then
 	echo "Apps with plate id or street ${PARAM}:"
-	for key in $(ssh nieradka.net "${SQL} \"select key, json_extract(value, '$.user.email'), json_extract(value, '$.statements.gallery') from applications where lower(json_extract(value, '$.carInfo.plateId')) like lower('%${PARAM}%') or lower(json_extract(value, '$.address.address')) like lower('%${PARAM}%') order by json_extract(value, '$.added') desc limit 100 \""); do
+	for key in $(ssh nieradka.net "${SQL} \"select key, json_extract(value, '$.user.email'), json_extract(value, '$.carInfo.plateId') from applications where lower(json_extract(value, '$.carInfo.plateId')) like lower('%${PARAM}%') or lower(json_extract(value, '$.address.address')) like lower('%${PARAM}%') order by json_extract(value, '$.added') desc limit 100 \""); do
 		print_app_url "${key%%|*}" "(${key#*|})"
 	done
 	exit 0
@@ -59,8 +59,6 @@ echo "Checking user ${PARAM}:";
 ssh nieradka.net "${SQL} \"select value from users where ${WHERE1} \"" | jq '.'
 echo "Stats:"
 ssh nieradka.net "${SQL} \"select value from applications where ${WHERE2} \"" | jq -r '.status' | sort | uniq -c | sort -nr
-echo "Gallery stats:"
-ssh nieradka.net "${SQL} \"select case when json_extract(value, '$.statements.gallery') != 0 then 'zgoda' else 'brak zgody' end as statement, case json_extract(value, '$.addedToGallery') = 0 when 1 then 'odrzucone' when 0 then 'zaakceptowane' else '   ' end as decyzja, count(*) from applications where ${WHERE2} group by 1, 2 ;\"" | tr "|" "	" | sed 's/^/\t/'
 
 echo "Last 10 applications:"
 for key in $(ssh nieradka.net "${SQL} \"select key, json_extract(value, '$.status') from applications where ${WHERE2} and json_extract(value, '$.status') != 'draft' order by json_extract(value, '$.added') desc limit 10 \""); do
