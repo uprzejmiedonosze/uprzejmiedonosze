dba1b354b970da3f1daed33a1677a21fc2493c1a
SZN <szymon@nieradka.net>
2025-01-08 19:54:04 +0000

    phpunit warning to exception

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,3 +10,8 @@ $GLOBALS['SM_ADDRESSES'] = $SM_ADDRESSES;
 $GLOBALS['CATEGORIES'] = $CATEGORIES;
 $GLOBALS['cache'] = $cache;
 $_SERVER['HTTP_USER_AGENT'] = 'PHPUnit';
+
+function exception_error_handler(int $errno, string $errstr, string $errfile = null, int $errline) {
+    throw new \ErrorException($errstr, 0, $errno, $errfile, $errline);
+}
+set_error_handler(exception_error_handler(...));
