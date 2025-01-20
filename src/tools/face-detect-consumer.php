1cea3d6193a4f1c32d1365f73171074a58fa8da7
SZN <szymon@nieradka.net>
2025-01-20 20:38:43 +0000

    face-detect consumer

--- src/tools/face-detect-consumer.php
+++ src/tools/face-detect-consumer.php
@@ -7,17 +7,17 @@ require_once(__DIR__ . '/../inc/integrations/curl.php');
 logger("Starting face-blur-consumer...", true);
 
 $consumer = function (string $appId): void {
-  $application = \app\get($appId);
-
-  $filename = ROOT . $application->contextImage->url;
-  $url = "http://localhost:2000/detect/$filename";
-  $faces = \curl\request($url, [], "FaceRecogniton");
-  $application->faces = $faces;
-
-  logger($filename);
-  logger($faces);
-  \app\save($application);
-  sleep(1);
+  try {
+    $application = \app\get($appId);
+    $filename = ROOT . $application->contextImage->url;
+    $url = "http://localhost:2000/detect/$filename";
+    $faces = \curl\request($url, [], "FaceRecogniton");
+    $application->faces = $faces;
+    \app\save($application);
+    sleep(5);
+  } catch (\Exception $e) {
+    logger("ERROR: Failed detect face in $appId " . $e->getMessage(), true);
+  }
 };
 
 consume($consumer);
