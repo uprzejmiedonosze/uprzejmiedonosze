74f856def8a369bf40136edd9b422f8e0e330661
SZN <szymon@nieradka.net>
2025-02-10 21:42:49 +0100

    config.dev.php template

---
+++ config.dev.php
@@ -0,0 +1,15 @@
+<?php
+define('PLATERECOGNIZER_SECRET', 'contact author');
+define('OPEN_ALPR_SECRET_1', 'contact author');
+define('OPEN_ALPR_SECRET_2', 'contact author');
+define('MAPBOX_API_TOKEN', 'contact author');
+define('GOOGLE_MAPS_API_TOKEN', 'contact author');
+
+define('MAILER_FROM', 'u@dka.email');
+
+define('CRYPTO_KEY', '7700bcc0327517849e966dd169791439');
+define('CRYPTO_IV', '330adc20ed7dcf8561ff4869dd434b85');
+define('CRYPTO_TAG', '190181339ab13a971415e977b736053f');
+
+
+?>
