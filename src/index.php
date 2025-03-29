af85f721f4aeddb0ce10e1c3f4731cac6f1bef6a
SZN <szymon@nieradka.net>
2025-03-29 15:40:46 +0100

    fix Undefined constant "HOST"

--- src/index.php
+++ src/index.php
@@ -3,6 +3,7 @@
 date_default_timezone_set('Europe/Warsaw');
 
 require_once(__DIR__ . '/../inc/Logger.php');
+require(__DIR__ . '/../inc/include.php');
 
 /**
  * @SuppressWarnings(PHPMD.Superglobals)
@@ -24,8 +25,6 @@ if (session_status() == PHP_SESSION_NONE && !isset($_GET["sessionless"])) {
     restore_error_handler();
 }
 
-
-require(__DIR__ . '/../inc/include.php');
 require(__DIR__ . '/../inc/Twig.php');
 require(__DIR__ . '/../inc/handlers/index.php');
 require(__DIR__ . '/../inc/middleware/index.php');
