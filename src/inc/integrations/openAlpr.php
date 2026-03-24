0a777a1fc15f8c46bb3196b050c838898b87e236
SZN <szymon@nieradka.net>
2026-03-24 14:28:38 +0100

    exec -> curl_init

--- src/inc/integrations/openAlpr.php
+++ src/inc/integrations/openAlpr.php
@@ -88,7 +88,7 @@ function get_alpr(&$imageBytes){
 
 function get_alpr_cli($imagePath) {
     logger("  get_alpr_cli");
-    $response = shell_exec("alpr --country eu --topn 1 --json " . ROOT . "$imagePath 2>/dev/null");
+    $response = shell_exec("alpr --country eu --topn 1 --json " . escapeshellarg(ROOT . $imagePath) . " 2>/dev/null");
     $json = json_decode($response, true);
     if(!json_last_error() === JSON_ERROR_NONE){
         return null;
